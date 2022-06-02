d3594d375a2007550f0b1da732cad0a92a1ecb34
AyshaAlessandra <aysha.alessandra.oliver@gmail.com>
2022-06-02 11:35:16 -0300

    adicionando métodos dto

--- APIcommerce-trabalhoFinal/src/main/java/com/residencia/commerce/service/ProdutoService.java
+++ APIcommerce-trabalhoFinal/src/main/java/com/residencia/commerce/service/ProdutoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.residencia.commerce.dto.ProdutoDTO;
 import com.residencia.commerce.entity.Produto;
 import com.residencia.commerce.repository.ProdutoRepository;
 
@@ -34,4 +35,36 @@ public class ProdutoService {
 		produtoRepository.delete(produto);
 
 	}
+
+	private Produto ConverteDTOToEntidade(ProdutoDTO produtoDTO) {
+		Produto produto = new Produto();
+
+		produto.setIdProduto(produtoDTO.getIdProduto());
+		produto.getCategoria().setIdCategoria(produtoDTO.getCategoriaDTO().getIdCategoria());
+		produto.setDataCadastroProduto(produtoDTO.getDataCadastroProduto());
+		produto.setDescricao(produtoDTO.getDescricao());
+		produto.setNomeImagemProduto(produtoDTO.getNomeImagemProduto());
+		produto.setNomeProduto(produtoDTO.getNomeProduto());
+		produto.setQtdEstoqueProduto(produtoDTO.getQtdEstoqueProduto());
+		produto.setValorUnitarioProduto(produtoDTO.getValorUnitarioProduto());
+
+		return produto;
+	}
+
+	private ProdutoDTO converterEntityToDTO(Produto produto) {
+		ProdutoDTO produtoDTO = new ProdutoDTO();
+
+		produtoDTO.getCategoriaDTO().setIdCategoria(produto.getCategoria().getIdCategoria());
+		produtoDTO.setDataCadastroProduto(produto.getDataCadastroProduto());
+		produtoDTO.setDescricao(produto.getDescricao());
+		produtoDTO.setIdProduto(produto.getIdProduto());
+		produtoDTO.setNomeImagemProduto(produto.getNomeImagemProduto());
+		produtoDTO.setNomeProduto(produto.getNomeProduto());
+		produtoDTO.setQtdEstoqueProduto(produto.getQtdEstoqueProduto());
+		produtoDTO.setValorUnitarioProduto(produto.getValorUnitarioProduto());
+
+		return produtoDTO;
+
+	}
+
 }
